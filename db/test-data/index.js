6463ffaffe79d157c5b0acfdd860de856d0ffd6a
ashler2 <ashley.rt@live.co.uk>
2019-06-24 11:41:08 +0100

    exporting data around db

---
+++ db/test-data/index.js
@@ -0,0 +1,4 @@
+exports.articlesData = require("./articles.js");
+exports.commentsData = require("./comments");
+exports.topicsData = require("./topics.js");
+exports.usersData = require("./users");
